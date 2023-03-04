1641b52eba9bffe74ea003aa7b6467956e71e23b
BazantU <mateusz.maleszyk@op.pl>
2023-03-04 18:03:56 +0100

    ostatnie

--- Fest PP Projekt/Assets/Itemy.cs
+++ Fest PP Projekt/Assets/Itemy.cs
@@ -38,14 +38,41 @@ public class Itemy : MonoBehaviour
             && hit.distance <= odleglosc)
         {   
             trzyma_przedmiot = true;
+            reka_gracza.localPosition = new Vector3(0.474f, -0.418f, 0.964f); //podstawowa wartosc
+            reka_gracza.localRotation = Quaternion.Euler(0f, 0f, 0f); //podstawowa wartosc
+
             var item = hit.transform;
             item.GetComponent<Outline>().enabled = false;
-
             item.GetComponent<Rigidbody>().isKinematic = true;
             item.SetParent(reka_gracza, true);
 
             item.localPosition = new Vector3(0f, 0f, 0f);
             item.localRotation = Quaternion.Euler(Vector3.zero);
+
+            //Specjalne przedmioty
+            if(item.name == "Papier7312"
+            || item.name == "annn"
+            || item.name == "re"
+            || item.name == "gm")
+            {reka_gracza.localRotation = Quaternion.Euler(-90f, 0f, 0f);}
+
+            if(item.name == "kartkasejf"
+            || item.name == "jaszczomb3"
+            || item.name == "werre")
+            {reka_gracza.localRotation = Quaternion.Euler(-90f, 180f, 0f);}
+
+            if(item.name == "Jagiello"
+            || item.name == "Wielkie_czyny"
+            || item.name == "Mieszko"
+            || item.name == "Szarza"
+            || item.name == "Rybak"
+            || item.name == "Chrobry")
+            {reka_gracza.localRotation = Quaternion.Euler(180f, 0f, 180f);
+            reka_gracza.localPosition = new Vector3(0.474f, -0.234f, 0.964f);} //-0.234
+
+            if(item.name == "xxx5"
+            || item.name == "kawalek_kodu")
+            {reka_gracza.localRotation = Quaternion.Euler(90f, 0f, 180f);}
         }
 
         if(Input.GetKeyDown(KeyCode.Q) && trzyma_przedmiot == true)
@@ -58,10 +85,12 @@ public class Itemy : MonoBehaviour
                     && wyrzuc_przedmiot.GetComponent<Outline>() != null
                     )
                 {   
-                    wyrzuc_przedmiot.GetComponent<Outline>().enabled = false;      
+                    wyrzuc_przedmiot.GetComponent<Outline>().enabled = false;
                     wyrzuc_przedmiot.SetParent(null);
                     wyrzuc_przedmiot.GetComponent<Rigidbody>().isKinematic = false;
 
+                    reka_gracza.localPosition = new Vector3(0.474f, -0.418f, 0.964f); //podstawowa wartosc
+                    reka_gracza.localRotation = Quaternion.Euler(0f, 0f, 0f); //podstawowa wartosc
                     trzyma_przedmiot = false;
                 }
             }
